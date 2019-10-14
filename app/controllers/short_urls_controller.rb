73d7392292f032758c107234d054138d1463e84e
Spencer Tassone <spencerwilliamtassone@gmail.com>
2019-10-14 00:23:49 -0400

    index and create in short URL controller

--- app/controllers/short_urls_controller.rb
+++ app/controllers/short_urls_controller.rb
@@ -1,15 +1,26 @@
+require "base62-rb"
+
 class ShortUrlsController < ApplicationController
 
   # Since we're working on an API, we don't have authenticity tokens
   skip_before_action :verify_authenticity_token
 
   def index
+    shortys = ShortUrl.all.order(:click_count)
+    render json: shortys
   end
 
   def create
+    begin
+      shorty = ShortUrl.get_or_create_from_params(create_params)
+      render json: shorty
+    rescue NoMethodError
+      render json: {error: "Invalid"}, status: :unprocessable_entity
+    end
   end
 
   def show
   end
+
 
 end
